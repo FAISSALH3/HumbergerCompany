a084340711626d45089a02366c46bc26050c943b
faissal hamdi <faissalhamdi@hotmail.com>
2020-12-17 17:31:38 -0300

    my first commit

---
+++ src/com/Faissal/Main.java
@@ -0,0 +1,8 @@
+package com.Faissal;
+
+public class Main {
+    public static void main(String[] args) {
+        com.Faissal.BasicHamburger basicOne = new com.Faissal.BasicHamburger("brown", "porc", true, true, false, true);
+        basicOne.priceTotal();
+    }
+}
